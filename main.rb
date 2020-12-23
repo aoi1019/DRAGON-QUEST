a35d129756e4acff283af334b50be0fde85bc8ed
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-23 19:37:30 +0900

    Update Main.rb

--- main.rb
+++ main.rb
@@ -1,36 +1,36 @@
-class Brave
-  def name=(name)
-    @name = name
-  end
+require './brave'
+require './monster'
 
-  def name
-    @name
-  end
+brave = Brave.new(name: "テリー", hp: 1000, offense: 50, defense: 20)
+monster = Monster.new(name: "スライム", hp: 100, offense: 50, defense: 20)
 
-  def hp=(hp)
-    @hp = hp
-  end
-  
-  def hp
-    @hp
-  end
+puts <<~TEXT
+NAME:#{brave.name}
+HP:#{brave.hp}
+OFFENSE:#{brave.offense}
+DEFENSE:#{brave.defense}\n
+TEXT
 
-  def offense=(offense)
-    @offense = offense
-  end
-
-  def offense
-    @offense
-  end
-
-  def defense=(defense)
-    @defense = defense
-  end
+loop do
+  brave.attach(monster)
+  break if monster.hp <= 0
+  monster.attack(brave)
+  break if brave.hp <= 0
 end
 
-brave = Brave.new
+battle_result = brave.hp > 0
 
-brave.name = "テリー"
-brave.hp = 1000
-brave.offense = 100
-brave.defense = 100
+if battle_result
+  exp = (monster.offense + monster.defense) * 2
+  gold = (monster.offense + monster.defense) * 3
+  puts <<~TEXT
+  #{monster.name}を倒した
+  #{brave.name}は戦いに勝った
+  #{exp}の経験値と#{gold}ゴールドを獲得した
+  TEXT
+else
+  puts <<~TEXT
+  #{brave.name}は戦いに負けた
+  目の前が真っ暗になった
+  TEXT
+end
