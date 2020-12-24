179a3eb900a49b904deb40a882fbf36f6810d32f
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-12-24 15:04:07 +0900

    Create GamesController.rb

---
+++ games_controller.rb
@@ -0,0 +1,51 @@
+class GamesController
+  
+  EXP_CONSTANT  = 2
+  GOLD_CONSTANT = 3
+  
+  def battle(**params)
+    build_characters(params)
+    
+    loop do
+      @brave.attach(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
+    end
+    battle_judgment
+  end
+
+  private
+
+    def build_characters(**params)
+      @brave = params[:brave]
+      @monster = params[:monster]
+    end
+
+    def battle_end?
+      @brave.hp <= 0 || @monster.hp <= 0
+    end
+
+    def battle_win?
+      @brave.hp > 0
+    end
+
+    def battle_judgment
+      if battle_win?
+        result = calculate_of_exp_and_gold
+        puts "#{@monster.name}を倒し、#{@brave.name}は戦いに勝った"
+        puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
+      else
+        puts "#{@brave.name}はたたかいに負けた"
+        puts "目の前が真っ暗になった"
+      end
+    end
+
+    def calculate_of_exp_and_gold
+      exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+      gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
+      result = {exp: exp, gold: gold}
+
+      result
+    end
+end
